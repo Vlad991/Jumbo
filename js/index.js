37dca5744079e3d6462cb9c804763e73b3c07e38
Vlad991 <vladkuzma99@gmail.com>
2020-02-27 14:22:47 +0200

    Add adaptive account

--- js/index.js
+++ js/index.js
@@ -13,6 +13,43 @@ $(function () {
             $menu.fadeTo(100, 0);
             setTimeout(function () {$menu.hide()}, 100);
         }
-    })
+    });
+
+    $('#loginButton').click(function () {
+        $('#loginIcon').toggle();
+    });
+
+    $('.container__login').click(function () {
+        $(this).hide();
+    });
+
+    $('.container__login form.login').click(function (event) {
+        event.stopPropagation();
+    });
+
+    $('form.login .login__cross').click(function () {
+        $('.container__login').hide();
+    });
+
+    $('.account-sidebar .account-sidebar__item').click(function (event) {
+        event.preventDefault();
+        let $this = $(this);
+        let tabName = $this.data('tab');
+        $('.account-sidebar .account-sidebar__item').removeClass('account-sidebar__item_active');
+        $this.addClass('account-sidebar__item_active');
+        let $tab = $('.account .account-body main[data-tab="' + tabName + '"]');
+        $('.account .account-body main').hide();
+        $tab.show();
+    });
+
+    $('.account-main .account-main__list .account-main__list-link').click(function (event) {
+        event.preventDefault();
+        let $this = $(this);
+        let tabName = $this.data('tab');
+        $('.account-sidebar .account-sidebar__item').removeClass('account-sidebar__item_active');
+        let $tab = $('.account .account-body main[data-tab="' + tabName + '"]');
+        $('.account .account-body main').hide();
+        $tab.show();
+    });
 
 });
